c07c49d1eeea76eef9da20bee31b8b1ff10f0f6e
Kenji Lao <lao.kenjii@gmail.com>
2022-02-11 11:53:21 +0100

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ConnexionUserComponent } from './connexion-user/connexion-user.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ConnexionUserComponent } from './connexion-user/connexion-user.componen
     ConnexionUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
