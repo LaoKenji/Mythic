ddf4017c8b13de28c2c17f772a2bb087542781a7
Kenji Lao <lao.kenjii@gmail.com>
2022-02-22 17:29:52 +0100

    fix connexion and inscription navigation

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
